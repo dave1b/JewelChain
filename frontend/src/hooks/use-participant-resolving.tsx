79cf885c60cf3dca3509187fcfd8cb535704aaab
sandro-salzmann <salzmann2k@gmail.com>
2022-11-24 03:51:42 +0100

    fix warning

--- frontend/src/hooks/use-participant-resolving.tsx
+++ frontend/src/hooks/use-participant-resolving.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { getParticipantInformation, ParticipantInformation } from '../api/queries/get-participant-information';
 import { address } from '../api/type';
